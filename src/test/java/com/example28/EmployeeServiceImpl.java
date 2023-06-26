1a3ec775d464fcbaa3d53bda27405b6872f6974a
робот <vaginalnaymamba@mail.ru>
2023-06-26 18:03:37 +0300

    finish hm

--- src/test/java/com/example28/EmployeeServiceImpl.java
+++ src/test/java/com/example28/EmployeeServiceImpl.java
@@ -3,14 +3,14 @@ package com.example28;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    final int id = 10;
     List<Employee> staff = new ArrayList<>(List.of(
-            new Employee("Чернов", "Данила", 60_000, 2),
+            new Employee("Максимов", "Данила", 60_000, 2),
             new Employee("Григорьев", "Олег", 50_000, 4),
             new Employee("Беляев", "Максим", 70_000, 2),
             new Employee("Александров", "Кирилл", 90_000, 1)
@@ -18,48 +18,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee minimumSalary(int departmentId) {
-        ArrayList<Employee> findedList = (ArrayList<Employee>) staff
-                .stream()
+        return  staff.stream()
                 .filter(d -> (d.getDepartment() == departmentId))
-                .collect(Collectors.toList());
-        int salary = findedList.get(0).getSalary();
-        int id=0;
-        for (int i = 0; i < findedList.size(); i++) {
-            if (findedList.get(i).getSalary() < salary) {
-                salary=findedList.get(i).getSalary();
-                id=i;
-            }
-        }
-
-        return findedList.get(id);
+                .min(Comparator.comparing(Employee::getSalary))
+                .get();
     }
 
     @Override
     public Employee maxSalary(int departmentId) {
-        ArrayList<Employee> findedList = (ArrayList<Employee>) staff
-                .stream()
+        return staff.stream()
                 .filter(d -> (d.getDepartment() == departmentId))
-                .collect(Collectors.toList());
-
-        int salary = findedList.get(0).getSalary();
-        int id=0;
-        for (int i = 0; i < findedList.size(); i++) {
-            if (findedList.get(i).getSalary() > salary) {
-                salary=findedList.get(i).getSalary();
-                id=i;
-            }
-        }
-
-        return findedList.get(id);
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
     }
 
     @Override
     public ArrayList<Employee> departmentsAll(int departmentId) {
-        ArrayList<Employee> findedList = (ArrayList<Employee>) staff
+        return (ArrayList<Employee>) staff
                 .stream()
                 .filter(d -> (d.getDepartment() == departmentId))
                 .collect(Collectors.toList());
-        return findedList;
+
     }
 
     @Override
